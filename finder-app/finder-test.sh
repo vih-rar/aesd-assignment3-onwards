c60b64ef9b5d90351585580fda3856251c25741f
Vihar Vasavada <vvasavada605@gmail.com>
2026-01-03 02:19:39 -0500

    Modifications for buildroot

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,7 +7,7 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
+WRITEDIR=/tmp/assignment4-result.txt
 username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
